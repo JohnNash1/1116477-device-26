719670318a9c1ea7d9069e759b147c929dad0c65
JohnNash1 <mihailshtv@gmail.com>
2019-10-10 00:56:18 +0500

    добавил ссылку для кнопки "Напишите нам" на случай, если не сработает js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
-const button = document.querySelector(".feedback-button");
+const button = document.querySelector("#writeus");
 const popup = document.querySelector(".feedback");
 const feedback = popup.querySelector("#username");
-const open = function () {
-  // evt.preventDefault();
+const open = function (evt) {
+  evt.preventDefault();
   popup.classList.add("feedback-show");
   feedback.focus();
 };
-
 const buttonClose = document.querySelector(".modal-close-button");
 const close = function () {
   popup.classList.remove("feedback-show");
@@ -22,27 +21,3 @@ const escClose = function (evt) {
 button.addEventListener("click", open);
 buttonClose.addEventListener("click", close);
 window.addEventListener("keydown", escClose);
-
-const map = document.querySelector(".map-img");
-const popupMap = document.querySelector(".modal-map");
-const mapFrame = popupMap.querySelector("iframe");
-const mapOpen = function (evt) {
-  evt.preventDefault();
-  popupMap.classList.add("modal-map-show");
-  mapFrame.focus();
-};
-const buttonMapClose = document.querySelector(".map-close-button");
-const mapClose = function () {
-  popupMap.classList.remove("modal-map-show");
-};
-const mapEscClose = function (evt) {
-  if (evt.keyCode === 27) {
-    if (popupMap.classList.contains("modal-map-show")) {
-      evt.preventDefault();
-      popupMap.classList.remove("modal-map-show");
-    }
-  }
-};
-map.addEventListener("click", mapOpen);
-window.addEventListener("keydown", mapEscClose);
-buttonMapClose.addEventListener("click", mapClose);
