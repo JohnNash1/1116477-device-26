6eac017c840c92bec5d2ebf90c99b2a404ab80de
JohnNash1 <mihailshtv@gmail.com>
2019-10-09 21:16:02 +0500

    добавил ссылку для телефона

--- js/script.js
+++ js/script.js
@@ -31,11 +31,18 @@ const mapOpen = function (evt) {
   popupMap.classList.add("modal-map-show");
   mapFrame.focus();
 };
-
 const buttonMapClose = document.querySelector(".map-close-button");
 const mapClose = function () {
   popupMap.classList.remove("modal-map-show");
 };
-
+const mapEscClose = function (evt) {
+  if (evt.keyCode === 27) {
+    if (popupMap.classList.contains("modal-map-show")) {
+      evt.preventDefault();
+      popupMap.classList.remove("modal-map-show");
+    }
+  }
+};
 map.addEventListener("click", mapOpen);
+window.addEventListener("keydown", mapEscClose);
 buttonMapClose.addEventListener("click", mapClose);
