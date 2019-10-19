d811874a2bb30219cb8a42a1003837621ad349cf
JohnNash1 <mihailshtv@gmail.com>
2019-10-19 08:01:06 +0500

    минимизация

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
-const button = document.querySelector(".writeus");
-const popup = document.querySelector(".feedback");
-const feedback = popup.querySelector("#username");
-const open = function (evt) {
+var button = document.querySelector(".writeus");
+var popup = document.querySelector(".feedback");
+var feedback = popup.querySelector("#username");
+var open = function (evt) {
   evt.preventDefault();
   popup.classList.add("feedback-show");
   feedback.focus();
 };
-const buttonClose = document.querySelector(".modal-close-button");
-const close = function () {
+var buttonClose = document.querySelector(".modal-close-button");
+var close = function () {
   popup.classList.remove("feedback-show");
 };
-const escClose = function (evt) {
+var escClose = function (evt) {
   if (evt.keyCode === 27) {
     if (popup.classList.contains("feedback-show")) {
       evt.preventDefault();
@@ -22,18 +22,18 @@ button.addEventListener("click", open);
 buttonClose.addEventListener("click", close);
 window.addEventListener("keydown", escClose);
 
-const map = document.querySelector(".map-img");
-const popupMap = document.querySelector(".modal-map");
-const mapFrame = popupMap.querySelector("iframe");
-const mapOpen = function (evt) {
+var map = document.querySelector(".map-img");
+var popupMap = document.querySelector(".modal-map");
+var mapFrame = popupMap.querySelector("iframe");
+var mapOpen = function (evt) {
   evt.preventDefault();
   popupMap.classList.add("modal-map-show");
 };
-const buttonMapClose = document.querySelector(".map-close-button");
-const mapClose = function () {
+var buttonMapClose = document.querySelector(".map-close-button");
+var mapClose = function () {
   popupMap.classList.remove("modal-map-show");
 };
-const mapEscClose = function (evt) {
+var mapEscClose = function (evt) {
   if (evt.keyCode === 27) {
     if (popupMap.classList.contains("modal-map-show")) {
       evt.preventDefault();
